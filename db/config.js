a438856c6fba964284b876113731bbb9043148cf
charana555 <charanchandrashekar55@gamil.com>
2022-02-26 12:49:19 +0530

    shifted it to online database

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
- const DB = 'mongodb://localhost:27017/sih'
+ const DB = 'mongodb+srv://Charan17:Charan555@cluster0.dezau.mongodb.net/sihbackend?retryWrites=true&w=majority'
 
 mongoose.Promise = global.Promise ;
 
